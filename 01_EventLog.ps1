bdc53786d8897917305966ca3de3b08d90bc4b07
calucas27 <chaselucas265@gmail.com>
2018-02-18 22:39:54 -0600

    Add files via upload

---
+++ 01_EventLog.ps1
@@ -0,0 +1,26 @@
+﻿<#
+    .SYNOPSIS
+    SCRIPT 01: Event Log Manipulation: By Chase Lucas
+
+    .EXAMPLE
+    .\01_EventLog.ps1
+    
+    
+    .DESCRIPTION
+    Prints the list of available event logs, creates a new event log named CSC443 with a maximum size of 64 KB,
+    1 day of retention, and an overflow action of OverwriteOlder.  100 logs will be written before the event
+    log list is printed again.  The existing logs will be exported to a .csv file before the event log is cleared.
+#>
+
+Get-EventLog -List
+New-EventLog -Source Script -logname "CSC443"
+Limit-EventLog -LogName "CSC443" -MaximumSize 64 -RetentionDays 1 -OverflowAction OverwriteOlder
+
+for($i=0; $i -lt 100; $i++){
+    Write-EventLog -LogName "CSC443" -Source "CSC443" -EventId 1001 -EntryType Information -Message "Log from Script!"
+}
+Get-EventLog -List
+Get-EventLog "CSC443" | Export-Csv eventlog.csv
+Clear-EventLog "CSC443"
+
+
---
+++ 03_CompInfo.ps1
@@ -0,0 +1,35 @@
+﻿<#
+    .SYNOPSIS
+    SCRIPT 03: Computer Status Details: By Chase Lucas
+
+    .EXAMPLE
+    .\03_CompInfo.ps1
+    
+    .DESCRIPTION
+    Gathers information about the computer, and stores the results in a custom object.
+#>
+$database = @()
+$computerinfo = New-Object System.Object
+
+$comp_name = $env:computername
+$comp_hostname = hostname
+$comp_process = Get-Process | Measure | Select-Object -ExpandProperty Count
+$comp_service = Get-Service | Measure | Select-Object -ExpandProperty Count
+$comp_servicestopped = Get-Service | Where-Object {$_.Status -eq "Stopped"} | Measure | Select-Object -ExpandProperty Count
+$comp_date = date
+$comp_os = Get-CimInstance Win32_OperatingSystem | Select-Object -ExpandProperty Version
+$comp_cpu = Get-CimInstance CIM_PROCESSOR | Select-Object -ExpandProperty Name
+
+
+$computerinfo | Add-Member -Type NoteProperty -Name comp_name -Value $comp_name
+$computerinfo | Add-Member -Type NoteProperty -Name comp_hostname -Value $comp_hostname
+$computerinfo | Add-Member -Type NoteProperty -Name comp_process -Value $comp_process
+$computerinfo | Add-Member -Type NoteProperty -Name comp_service -Value $comp_service
+$computerinfo | Add-Member -Type NoteProperty -Name comp_servicestopped -Value $comp_servicestopped
+$computerinfo | Add-Member -Type NoteProperty -Name comp_date -Value $comp_date
+$computerinfo | Add-Member -Type NoteProperty -Name comp_os -Value $comp_os
+$computerinfo | Add-Member -Type NoteProperty -Name comp_cpu -Value $comp_cpu
+
+$database += $computerinfo
+
+Write-Host ($computerinfo | Format-List | Out-String)
