a02f2695ca3afe5609ecd48b5d09deb5ad511d7a
calucas27 <chaselucas265@gmail.com>
2018-03-05 20:07:57 -0500

    Lab08

---
+++ passgen.ps1
@@ -0,0 +1,39 @@
+﻿<#
+    .SYNOPSIS
+    SCRIPT 02: Generate Passwords: By Chase Lucas
+
+    .EXAMPLE
+    ./genpass.ps1 -length 10 -user calucas
+    
+    .DESCRIPTION
+    Allows the user to enter in several different criteria to customize their passwords by.  Users may specify how many
+    characters they wish their passwords to be.  The script will select 8 by default, but this can be overridden.  Users may
+    also specify the complexity they wish for their passwords to be, by including parameters such as numbers, special characters,
+    etc.
+#>
+param(
+[Parameter(mandatory=$false)][int]$Length = 8,
+[Parameter(mandatory=$false)][switch]$Lowercase,
+[Parameter(mandatory=$false)][switch]$Uppercase,
+[Parameter(mandatory=$false)][switch]$Numbers,
+[Parameter(mandatory=$false)][switch]$Specialchars
+)
+$charset = ""
+$password = ""
+if($Lowercase.IsPresent -eq $true){
+    $charset = $charset += ("abcdefghijklmnopqrstuvwxyz")
+}
+if($Uppercase.IsPresent -eq $true){
+    $charset = $charset += ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+}
+if($Numbers.IsPresent -eq $true){
+    $charset = $charset += ("1234567890")
+}
+if($Specialchars.IsPresent -eq $true){
+    $charset = $charset += ("!@#$%^&*")
+}
+$charset = $charset.ToCharArray()
+for($i=0; $i -lt $Length; $i++){
+    $password += $charset | Get-Random
+}
+echo $password
